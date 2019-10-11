2996ee7c3d7bd84a6007743847ba383da7a380ec
charx <carlos.huertaso@udlap.mx>
2019-10-11 14:28:40 +0200

    basic setup of the functions of the code

--- install_packages.R
+++ install_packages.R
@@ -1,1 +1,8 @@
 install.packages("devtools")
+
+# Commands to install the LIMMA package
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("limma") # install the limma package
+BiocManager::install("ROC") # install the ROC package
---
+++ src/assignment3.R
@@ -0,0 +1,5 @@
+rm(list=ls()) # clean R environment
+cat("\f") # clear the console
+
+source('./utils.R')
+source('./MCMC.R')

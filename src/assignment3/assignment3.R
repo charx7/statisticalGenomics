ca6e85baa514174713a7668ffe323a90eca1f4e7
charx <carlos.huertaso@udlap.mx>
2019-10-13 12:51:09 +0200

    completed ex3

--- src/assignment3/assignment3.R
+++ src/assignment3/assignment3.R
@@ -5,30 +5,23 @@ source('./utils.R')
 source('./MCMC.R')
 
 #  Data Params
-m_obs     <- 20 # numb of observations
+m_obs     <- c(20, 50, 100) # numb of observations
 var_noise <- 1 # noise of the data
-data      <- make_test_Data(m_obs,var_noise) # generate data
-
-n_nodes   <- nrow(data)  #  no. of network nodes
 # MCMC params
 iterations  <- 20000 # Total no. of MCMC iterations per run
 step_save   <- 100 # Thin-out by a factor of 100
 
-# Generate an empty incidence matrix
-start_incidence <- matrix(0,n_nodes,n_nodes)
-
-# Exercise 3
-# Run the MCMC algo
-Sim <- strMCMC(data, start_incidence, iterations, step_save)
-n   <- length(Sim[[1]]) # Get length of the chain
-SCORES_undirected <- matrix(0,11,11) # declare an empty edge score matrix
-for (i in 1:n){
-  # Sum the number of apperances on each score
-  SCORES_undirected <- (SCORES_undirected + Sim[[1]][[i]] + t(Sim[[1]][[i]]))
+data.list = list()
+data.idx = 0
+for (idx in 1:3) {
+  for (jdx in 1:5) {
+    data.idx = data.idx + 1
+    cur.obs = m_obs[idx] # get the current numb of observations
+    cur.data <- make_test_Data(cur.obs,var_noise) # generate data
+    data.list[[data.idx]] = cur.data
+  }
 }
-SCORES_undirected <- SCORES_undirected/n # divivde by n to get the posterior edge score matrix
 
-true_Net <- make_true_Net() # get the true network structure
-true_Net <- true_Net + t(true_Net) # get also the simmetric true edges
-AUROC <- compute_AUROC(SCORES_undirected,true_Net) # calculate the AUC
-
+#curr_n_nodes <- nrow(data)  #  no. of network nodes
+# Generate an empty incidence matrix
+#start_incidence <- matrix(0,n_nodes,n_nodes)
---
+++ src/assignment3/exercise3.R
@@ -0,0 +1,36 @@
+# Exercise 3
+source('./assignment3.R')
+
+AUROC.vector = vector() # empty auroc scores list
+# Run MCMC for the 15 datasets
+for (idx in 1:15) {
+  progress(idx)
+  curr_data = data.list[[idx]]
+
+  n_nodes <- nrow(curr_data)  #  no. of network nodes
+  # Generate an empty incidence matrix
+  start_incidence <- matrix(0, n_nodes,n_nodes)
+
+  # Run the MCMC algo
+  Sim <- strMCMC(curr_data, start_incidence, iterations, step_save)
+  n   <- length(Sim[[1]]) # Get length of the chain
+  SCORES_undirected <- matrix(0,11,11) # declare an empty edge score matrix
+  for (i in 1:n){
+    # Sum the number of apperances on each score
+    SCORES_undirected <- (SCORES_undirected + Sim[[1]][[i]] + t(Sim[[1]][[i]]))
+  }
+  SCORES_undirected <- SCORES_undirected/n # divivde by n to get the posterior edge score matrix
+
+  true_Net <- make_true_Net() # get the true network structure
+  true_Net <- true_Net + t(true_Net) # get also the simmetric true edges
+  AUROC <- compute_AUROC(SCORES_undirected,true_Net) # calculate the AUC
+  AUROC.vector = c(AUROC.vector, AUROC) # append the current AUROC
+}
+# means computations
+cat('The mean of the AUROC of m=20 is: ', mean(AUROC.vector[1:5]))
+cat('The mean of the AUROC of m=50 is: ', mean(AUROC.vector[6:10]))
+cat('The mean of the AUROC of m=100 is: ', mean(AUROC.vector[11:15]))
+# std computations
+cat('The std of the AUROC of m=20 is: ', sqrt(var(AUROC.vector[1:5])))
+cat('The std of the AUROC of m=50 is: ', sqrt(var(AUROC.vector[6:10])))
+cat('The std of the AUROC of m=100 is: ', sqrt(var(AUROC.vector[11:15])))
